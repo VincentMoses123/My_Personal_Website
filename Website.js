2ebcdda89e29d6424993c6a56da846ac5a9803b1
VincentMoses123 <24.vincent.moses@gmail.com>
2024-08-10 16:28:21 -0400

    Added a back to top button!

--- Website.js
+++ Website.js
@@ -1,4 +1,20 @@
-function myFunction(){
-	alert("I Love ELISABETH CRANERT! She makes me so so happy and she has no idea... shes the best!");
-}
+    // Function to scroll to top
+    function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
 
+    // Show the button when the user scrolls down 20px
+    window.onscroll = function () {
+        var scrollTopBtn = document.getElementById("scrollTopBtn");
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            scrollTopBtn.style.display = "block";
+        } else {
+            scrollTopBtn.style.display = "none";
+        }
+    };
+
+    // Function for Elisabeth Cranert button
+    function myFunction() {
+        alert("Hello, Elisabeth Cranert! This message is just for you!");
+    }
